5bdb28e225943286df6fe9f8eb5f4b767bb4ba44
Jovan Bogdanović <51538656+jovandeka@users.noreply.github.com>
2023-01-18 21:41:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -248,7 +248,6 @@
     <script src="./JS/navbar.js"></script>
     <script src="./JS/display.js"></script>
     <script src="./JS/game.js"></script>
-    <script src="./JS/gallery.js"></script>
 
 </body>
 </html>
